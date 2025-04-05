0a3c1552d8c4a93b64bb0b31a47bd34708d64042
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-04-05 18:04:20 -0300

    add rest template to main

--- order/src/main/java/com/majesty/order/OrderApplication.java
+++ order/src/main/java/com/majesty/order/OrderApplication.java
@@ -2,6 +2,9 @@ package com.majesty.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class OrderApplication {
@@ -9,5 +12,12 @@ public class OrderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OrderApplication.class, args);
 	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
 
 }
