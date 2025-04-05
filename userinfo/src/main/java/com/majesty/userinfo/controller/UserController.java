5bfeffb5fa0d4a3a597db8ee90c441e3b57d9bce
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-04-05 08:42:01 -0300

    add user controller

---
+++ userinfo/src/main/java/com/majesty/userinfo/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.majesty.userinfo.controller;
+
+import com.majesty.userinfo.dto.UserDto;
+import com.majesty.userinfo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @PostMapping("/addUser")
+    public ResponseEntity<UserDto> addUser(@RequestBody UserDto userDto){
+        UserDto savedUser =  userService.addUser(userDto);
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/fetchUserById/{userId}")
+    public ResponseEntity<UserDto> fetchUserDetailsById(@PathVariable Integer userId){
+        return userService.fetchUserDetailsById(userId);
+    }
+
+
+
+}
