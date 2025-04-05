da7a667fc38b64c51f5fbe29a1fe14e2faac8ec3
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-04-05 16:24:23 -0300

    add food item entity

---
+++ foodcatalogue/src/main/java/com/majesty/foodcatalogue/entity/FoodItem.java
@@ -0,0 +1,32 @@
+package com.majesty.foodcatalogue.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FoodItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String itemName;
+
+    private String itemDescription;
+
+    private boolean isVegetable;
+
+    private Number price;
+
+    private Integer restaurantId;
+
+    @Column(nullable = false, columnDefinition = "INT DEFAULT 0")
+    private Integer quantity;
+}
