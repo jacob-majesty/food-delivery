ade87fee0876cceec957b2fc47a83753fdff2415
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-04-05 18:04:35 -0300

    add order mapper

---
+++ order/src/main/java/com/majesty/order/mapper/OrderMapper.java
@@ -0,0 +1,15 @@
+package com.majesty.order.mapper;
+
+import com.majesty.order.dto.OrderDto;
+import com.majesty.order.entity.Order;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface OrderMapper {
+
+    OrderMapper INSTANCE = Mappers.getMapper(OrderMapper.class);
+
+    Order mapOrderDtoToOrder(OrderDto orderDto);
+    OrderDto mapOrderToOrderDto(Order order);
+}
