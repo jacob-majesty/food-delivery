bb911f56210072be52a2de1e3c08417ceeb099fa
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-04-06 12:51:57 -0300

    add food-catalogue components

--- food-delivery-fe/src/app/app.module.ts
+++ food-delivery-fe/src/app/app.module.ts
@@ -4,10 +4,13 @@ import { HeaderComponent } from './header/components/header.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HeaderModule } from './header/header.module';
+import { RestaurantListingModule } from './restaurant-listing/restaurant-listing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FoodCatalogueModule } from './food-catalogue/food-catalogue.module';
 
 @NgModule({
   declarations: [],
-  imports: [RouterModule, BrowserModule, HeaderComponent, AppComponent, HeaderModule],
+  imports: [RouterModule, BrowserModule, HeaderComponent, AppComponent, HeaderModule, RestaurantListingModule, HttpClientModule,FoodCatalogueModule],
   providers: [],
   bootstrap: []
 })
