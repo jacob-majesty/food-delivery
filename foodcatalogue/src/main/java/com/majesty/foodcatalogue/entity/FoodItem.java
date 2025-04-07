bf4414feb05dc8c8aeb7604d85cff2a963171c59
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-04-06 23:48:50 -0300

    add Entity to FoodItem

--- foodcatalogue/src/main/java/com/majesty/foodcatalogue/entity/FoodItem.java
+++ foodcatalogue/src/main/java/com/majesty/foodcatalogue/entity/FoodItem.java
@@ -1,9 +1,6 @@
 package com.majesty.foodcatalogue.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +8,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class FoodItem {
 
     @Id
